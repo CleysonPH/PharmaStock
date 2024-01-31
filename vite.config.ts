241564302a27b16aea6e314cac7ecef9c333052d
Cleyson <cleysonph@gmail.com>
2024-01-31 19:39:42 -0300

    chore: add coverage exclusions config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,14 @@ import tsconfigpaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [tsconfigpaths()]
+  plugins: [tsconfigpaths()],
+  test: {
+    include: ['src/**/*.spec.ts'],
+    coverage: {
+      exclude: [
+        'build',
+        'src/server.ts'
+      ]
+    }
+  }
 });
