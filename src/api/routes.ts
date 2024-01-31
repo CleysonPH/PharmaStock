177eaab99b0a68a1bf5e5665034b3fb4a61a03e1
Cleyson <cleysonph@gmail.com>
2024-01-31 19:59:28 -0300

    refactor: use facotries to make PingController

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,9 +1,6 @@
+import { PingFacotry } from '@/config/factories/ping-factory';
 import { Router } from 'express';
-
-import { PingController } from './controllers/ping-controller';
 
 export const apiRoutes = Router();
 
-const pingController = new PingController();
-
-apiRoutes.get('/ping', pingController.ping);
+apiRoutes.get('/ping', PingFacotry.pingController.ping);
