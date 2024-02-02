3020f7df9bb8851e3650a4a9ec62670a3a070ec6
Cleyson <cleysonph@gmail.com>
2024-02-02 10:46:52 -0300

    fix: change message to trigger deploy

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from './app';
 import { env } from './config/env';
 
 app.listen(env.PORT, '0.0.0.0', () => {
-  console.log(`🚀 Server is running on port ${ env.PORT }.`);
+  console.log(`Server is running on port ${ env.PORT }.`);
 });
