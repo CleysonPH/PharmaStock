6b2b1d3a2a1d9758d00fd79f5558ce35308b22b5
Cleyson <cleysonph@gmail.com>
2024-02-02 09:30:32 -0300

    feat: add authentication

--- src/api/controllers/medicine-controller.spec.ts
+++ src/api/controllers/medicine-controller.spec.ts
@@ -1,15 +1,35 @@
 import { app } from '@/app';
 import { MedicineFactory } from '@/config/factories/medicine-factory';
+import { ServicesFactory } from '@/config/factories/services-factory';
+import { UserFactory } from '@/config/factories/user-factory';
 import { Medicine } from '@/core/domain/medicine-domain';
 import { InMemoryMedicineRepository } from '@/core/repositories/inmemory/in-memory-medicine-repository';
+import { InMemoryUserRepository } from '@/core/repositories/inmemory/in-memory-user-repository';
 import { randomUUID } from 'node:crypto';
 import request from 'supertest';
 import { beforeEach, describe, expect, it } from 'vitest';
 
 describe('MedicineController', async () => {
   let initialMedicines: Medicine[];
+  let token: string;
 
   beforeEach(async () => {
+    const passwordEncoder = ServicesFactory.passwordEncoderService;
+    const user = {
+      id: randomUUID(),
+      email: 'test@mail.com',
+      password: await passwordEncoder.encode('password')
+    };
+    const userRepository = UserFactory.userRepository as InMemoryUserRepository;
+    userRepository.users = [user];
+    const loginResponse = await request(app)
+      .post('/api/auth/login')
+      .send({
+        email: user.email,
+        password: 'password'
+      });
+    token = loginResponse.body?.token;
+
     initialMedicines = [
       {
         id: randomUUID(),
@@ -40,6 +60,7 @@ describe('MedicineController', async () => {
   it('should create a new medicine', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol',
         description: 'Painkiller',
@@ -59,6 +80,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if name is less than 3 characters', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Pa',
         description: 'Painkiller',
@@ -71,6 +93,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if description is less than 3 characters', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol',
         description: 'Pa',
@@ -83,6 +106,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if price is negative', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol',
         description: 'Painkiller',
@@ -95,6 +119,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if price is zero', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol',
         description: 'Painkiller',
@@ -107,6 +132,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if price is not a number', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol',
         description: 'Painkiller',
@@ -119,6 +145,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if name is not a string', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 10,
         description: 'Painkiller',
@@ -131,6 +158,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if description is not a string', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol',
         description: 10,
@@ -143,6 +171,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if name is more than 100 characters', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'a'.repeat(101),
         description: 'Painkiller',
@@ -155,6 +184,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if description is more than 255 characters', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol',
         description: 'a'.repeat(256),
@@ -167,6 +197,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if name is missing', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         description: 'Painkiller',
         price: 10
@@ -178,6 +209,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if description is missing', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol',
         price: 10
@@ -189,6 +221,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if price is missing', async () => {
     const response = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol',
         description: 'Painkiller'
@@ -200,6 +233,7 @@ describe('MedicineController', async () => {
   it('should return status 200 if medicine is updated', async () => {
     const creationResponse = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol',
         description: 'Painkiller',
@@ -210,6 +244,7 @@ describe('MedicineController', async () => {
 
     const response = await request(app)
       .put(`/api/medicines/${ id }`)
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol 500mg',
         description: 'Painkiller Edited',
@@ -229,6 +264,7 @@ describe('MedicineController', async () => {
   it('should return status 400 if id is not a uuid', async () => {
     const response = await request(app)
       .put('/api/medicines/123')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol 500mg',
         description: 'Painkiller Edited',
@@ -241,6 +277,7 @@ describe('MedicineController', async () => {
   it('should return status 404 if medicine is not found', async () => {
     const response = await request(app)
       .put(`/api/medicines/${ randomUUID() }`)
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol 500mg',
         description: 'Painkiller Edited',
@@ -252,9 +289,76 @@ describe('MedicineController', async () => {
 
   it('should return status 200 with all medicines', async () => {
     const response = await request(app)
-      .get('/api/medicines');
+      .get('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`);
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(initialMedicines);
+  });
+
+  it('should return status 401 if token is missing', async () => {
+    const response = await request(app)
+      .get('/api/medicines');
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should return status 401 if token is wrong type', async () => {
+    const response = await request(app)
+      .get('/api/medicines')
+      .set('Authorization', `Basic ${ token }`);
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should return status 401 if token is empty', async () => {
+    const response = await request(app)
+      .get('/api/medicines')
+      .set('Authorization', 'Bearer');
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should return status 401 if token is invalid', async () => {
+    const response = await request(app)
+      .get('/api/medicines')
+      .set('Authorization', `Bearer ${ randomUUID() }`);
+
+    expect(response.status).toBe(401);
+  });
+
+  it('it should return status 401 if token is missing on create', async () => {
+    const response = await request(app)
+      .post('/api/medicines')
+      .send({
+        name: 'Paracetamol',
+        description: 'Painkiller',
+        price: 10
+      });
+
+    expect(response.status).toBe(401);
+  });
+
+  it('it should return status 401 if token is missing on update', async () => {
+    const creationResponse = await request(app)
+      .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
+      .send({
+        name: 'Paracetamol',
+        description: 'Painkiller',
+        price: 10
+      });
+
+    const { id } = creationResponse.body;
+
+    const response = await request(app)
+      .put(`/api/medicines/${ id }`)
+      .send({
+        name: 'Paracetamol',
+        description: 'Painkiller',
+        price: 10
+      });
+
+    expect(response.status).toBe(401);
   });
 });
--- src/api/controllers/stock-movement-controller.spec.ts
+++ src/api/controllers/stock-movement-controller.spec.ts
@@ -1,13 +1,34 @@
 import { app } from '@/app';
+import { ServicesFactory } from '@/config/factories/services-factory';
 import { StockMovementFactory } from '@/config/factories/stock-movement-factory';
+import { UserFactory } from '@/config/factories/user-factory';
 import { StockMovementType } from '@/core/domain/stock-movement-domain';
 import { InMemoryStockMovementRepository } from '@/core/repositories/inmemory/in-memory-stock-movement-repository';
+import { InMemoryUserRepository } from '@/core/repositories/inmemory/in-memory-user-repository';
 import { randomUUID } from 'node:crypto';
 import request from 'supertest';
 import { beforeEach, describe, expect, it } from 'vitest';
 
 describe('StockMovementController', async () => {
+  let token: string;
+
   beforeEach(async () => {
+    const passwordEncoder = ServicesFactory.passwordEncoderService;
+    const user = {
+      id: randomUUID(),
+      email: 'test@mail.com',
+      password: await passwordEncoder.encode('password')
+    };
+    const userRepository = UserFactory.userRepository as InMemoryUserRepository;
+    userRepository.users = [user];
+    const loginResponse = await request(app)
+      .post('/api/auth/login')
+      .send({
+        email: user.email,
+        password: 'password'
+      });
+    token = loginResponse.body?.token;
+
     const medicines = [randomUUID(), randomUUID()];
     const stockMovements = [
       {
@@ -53,6 +74,7 @@ describe('StockMovementController', async () => {
   it('should create a new stock movement', async () => {
     const medicineResponse = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol',
         description: 'Painkiller',
@@ -63,6 +85,7 @@ describe('StockMovementController', async () => {
 
     const response = await request(app)
       .post('/api/stock-movements')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         medicineId,
         quantity: 10,
@@ -82,6 +105,7 @@ describe('StockMovementController', async () => {
   it('should return status 400 if medicineId is not a valid uuid', async () => {
     const response = await request(app)
       .post('/api/stock-movements')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         medicineId: 'invalid-uuid',
         quantity: 10,
@@ -94,6 +118,7 @@ describe('StockMovementController', async () => {
   it('should return status 400 if quantity is negative', async () => {
     const response = await request(app)
       .post('/api/stock-movements')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         medicineId: 'invalid-uuid',
         quantity: -10,
@@ -106,6 +131,7 @@ describe('StockMovementController', async () => {
   it('should return status 400 if type is not IN or OUT', async () => {
     const response = await request(app)
       .post('/api/stock-movements')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         medicineId: 'invalid-uuid',
         quantity: 10,
@@ -118,6 +144,7 @@ describe('StockMovementController', async () => {
   it('should return status 404 if medicineId does not exist', async () => {
     const response = await request(app)
       .post('/api/stock-movements')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         medicineId: randomUUID(),
         quantity: 10,
@@ -130,6 +157,7 @@ describe('StockMovementController', async () => {
   it('should return status 400 if medicine is out of stock', async () => {
     const medicineResponse = await request(app)
       .post('/api/medicines')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         name: 'Paracetamol',
         description: 'Painkiller',
@@ -140,6 +168,7 @@ describe('StockMovementController', async () => {
 
     const response = await request(app)
       .post('/api/stock-movements')
+      .set('Authorization', `Bearer ${ token }`)
       .send({
         medicineId,
         quantity: 10,
@@ -151,7 +180,8 @@ describe('StockMovementController', async () => {
 
   it('should return the stock movements report', async () => {
     const response = await request(app)
-      .get('/api/stock-movements/report');
+      .get('/api/stock-movements/report')
+      .set('Authorization', `Bearer ${ token }`);
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual({
@@ -203,7 +233,8 @@ describe('StockMovementController', async () => {
   it('should return the stock movements report filtered by date', async () => {
     const response = await request(app)
       .get('/api/stock-movements/report')
-      .query({ from: '2021-01-02', to: '2021-01-04' });
+      .query({ from: '2021-01-02', to: '2021-01-04' })
+      .set('Authorization', `Bearer ${ token }`);
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual({
@@ -241,8 +272,28 @@ describe('StockMovementController', async () => {
   it('should return status 400 if from is not a valid date', async () => {
     const response = await request(app)
       .get('/api/stock-movements/report')
-      .query({ from: 'invalid-date' });
+      .query({ from: 'invalid-date' })
+      .set('Authorization', `Bearer ${ token }`);
 
     expect(response.status).toBe(400);
+  });
+
+  it('should return status 401 if token is missing on stock movements report', async () => {
+    const response = await request(app)
+      .get('/api/stock-movements/report');
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should return status 401 if token is missing on stock movements creation', async () => {
+    const response = await request(app)
+      .post('/api/stock-movements')
+      .send({
+        medicineId: randomUUID(),
+        quantity: 10,
+        type: 'IN'
+      });
+
+    expect(response.status).toBe(401);
   });
 });
